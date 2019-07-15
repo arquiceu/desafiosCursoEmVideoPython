28efeac6185ac08aab9c9a5195927e30d651e52e
arquiceu <arquiceu@gmail.com>
2019-07-14 19:35:25 -0800

    Resposta do desafio 96

---
+++ desafio96.py
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Desafio 96 - Curso de Python do Canal Curso em Vídeo
+# https://youtu.be/oV1s53YGtvE
+
+
+def area(dimensao1, dimensao2):
+    print(f'A área de um terreno {dimensao1} x {dimensao2} é de {dimensao1 * dimensao2}m²')
+
+
+print('Controle de Terrenos\n','-' * 20)
+area(float(input('LARGURA (m): ')),float(input('COMPRIMENTO (m): ')))
