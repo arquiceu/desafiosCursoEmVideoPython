4a1b895c360f4b9e4c05d1e61e90b7ef63195a4d
arquiceu <arquiceu@gmail.com>
2019-07-14 20:32:25 -0800

    Resposta ao desafio 97

---
+++ desafio97.py
@@ -0,0 +1,13 @@
+#!/urs/bin/env python
+# -*- coding: utf-8 -*-
+# Desafio 97 - Curso de Python do Canal Curso em Vídeo
+# https://youtu.be/Q6basnSo7r0
+
+
+def escreva(mensagem):
+    print(f'{"~" * (len(mensagem) + 4)}\n  {mensagem}\n{"~" * (len(mensagem) + 4)}\n')
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
